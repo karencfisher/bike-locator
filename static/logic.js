15140b103044843fbeae687a371195ff0f7cc0e2
karencfisher <karencfisher@hotmail.com>
2023-12-19 21:20:52 -0700

    fix bug not updating new address

--- static/logic.js
+++ static/logic.js
@@ -8,8 +8,6 @@ function submitForm() {
 }
 
 function showMap(data) {
-    console.log(data);
-
     let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
